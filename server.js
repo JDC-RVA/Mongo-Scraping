0a7f0c87b767abe40b9163213a60dc549cfce932
JDC-RVA <jeremy@firstmotionlc.com>
2019-06-18 18:05:41 -0400

    updated mongodb connection with heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -28,7 +28,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articlesScraper", {
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/articlesScraper";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
